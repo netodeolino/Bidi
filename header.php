6c0ae8435e5267f48d27cc5be55e17df9e28f87b
netodeolino <netodeolino@gmail.com>
2015-10-18 23:17:57 -0300

    adjust images to width ok

--- header.php
+++ header.php
@@ -4,11 +4,10 @@
 
 	<nav class="menu-options">
 		<ul>
-			<li><a href="#">Home</a></li>
 			<li><a href="#">Articles</a></li>
 			<li><a href="#">Learning</a></li>
+			<li><a href="#">Development</a></li>
 			<li><a href="#">About</a></li>
-			<li><a href="#">Contact</a></li>
 		</ul>
 	</nav>
 
--- home.php
+++ home.php
@@ -5,15 +5,47 @@
 	<meta name="viewport" content="width=device-width">
 	<title>Bidi</title>
 	<link rel="stylesheet" href="css/home.css">
+	<link rel="stylesheet" href="css/jcarousel.basic.css">
+
+	<script type="text/javascript" src="js/modernizr-custom.js"></script>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<!--<script type="text/javascript" src="js/jcarousel.js"></script>-->
+	<script type="text/javascript" src="js/jcarousel.basic.js"></script>
+	
+	</script>
 </head>
 
 <body>
 	<?php include("header.php"); ?>
 	
 	<div class="container">
-		<p>Como vou organizar meu site?</p>
-	</div>
+<div class="wrapper">
+            <h1>Basic carousel</h1>
 
+            <p>This example shows how to setup a basic carousel with prev/next controls and pagination.</p>
+
+            <div class="jcarousel-wrapper">
+                <div class="jcarousel">
+                    <ul>
+                        <li><a href="http://globoesporte.com"><img src="img/one.jpg" width="600" height="400" alt=""></a></li>
+                        <li><img src="img/two.jpg" width="600" height="400" alt=""></li>
+                        <li><img src="img/three.png" width="600" height="400" alt=""></li>
+                    </ul>
+                </div>
+
+                <p class="photo-credits">
+                    Photos by <a href="http://www.mw-fotografie.de">Neto Deolino</a>
+                </p>
+
+                <a href="#" class="jcarousel-control-prev">&lsaquo;</a>
+                <a href="#" class="jcarousel-control-next">&rsaquo;</a>
+                
+                <p class="jcarousel-pagination">
+                    
+                </p>
+            </div>
+        </div>
+</div>
 	<?php include("footer.php"); ?>
 
 </body>
