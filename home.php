15c193a044993525f234dcbd40d814551cf8eeef
netodeolino <netodeolino@gmail.com>
2015-10-22 13:30:18 -0300

    restart

--- home.php
+++ home.php
@@ -5,13 +5,9 @@
 	<meta name="viewport" content="width=device-width">
 	<title>Bidi</title>
 	<link rel="stylesheet" href="css/home.css">
-	<link rel="stylesheet" href="css/jcarousel.basic.css">
 
 	<script type="text/javascript" src="js/modernizr-custom.js"></script>
 	<script type="text/javascript" src="js/jquery.js"></script>
-	<!--<script type="text/javascript" src="js/jcarousel.js"></script>-->
-	<script type="text/javascript" src="js/jcarousel.min.js"></script>
-	<script type="text/javascript" src="js/jcarousel.basic.js"></script>
 	
 	</script>
 </head>
@@ -20,34 +16,9 @@
 	<?php include("header.php"); ?>
 	
 	<div class="container">
-<div class="wrapper">
-            <h1>Teste banner rotativo</h1>
 
-            <p>This example shows how to setup a basic carousel with prev/next controls and pagination.</p>
-
-            <div class="jcarousel-wrapper">
-                <div class="jcarousel">
-                    <ul>
-                        <li><img src="img/one.jpg" width="600" height="400" alt=""></li>
-                        <li><img src="img/two.jpg" width="600" height="400" alt=""></li>
-                        <li><img src="img/three.png" width="600" height="400" alt=""></li>
-                    </ul>
-                </div>
-
-                <p class="photo-credits">
-                    Photos by <a href="http://www.mw-fotografie.de">Neto Deolino</a>
-                </p>
-
-                <a href="#" class="jcarousel-control-prev">&lsaquo;</a>
-                <a href="#" class="jcarousel-control-next">&rsaquo;</a>
-                
-                <p class="jcarousel-pagination">
-                    
-                </p>
-            </div>
-        </div>
-</div>
+	</div>
+	
 	<?php include("footer.php"); ?>
-
 </body>
 </html>
